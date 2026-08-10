1ca493ff15d4976f4daf5f144b190dfe7aecb9fe
Bhavya1820 <bhavyasoni1820@gmail.com>
2026-08-10 11:50:49 +0530

    intialize auth api

---
+++ server/controller/authController.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken"
+import nodemailer from "nodemailer"
+import User from "../models/User"
+import dotenv from "dotenv";
+
+export const requestOtp = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
+
+export const verifyOtp = async (req, res) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
